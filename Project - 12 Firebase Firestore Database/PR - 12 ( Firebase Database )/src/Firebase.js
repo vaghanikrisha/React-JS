4cc7de168337cc41d6dd4ee5d77851aab96f3abd
vaghanikrisha <vaghanikrisha546@gmail.com>
2024-10-16 20:27:36 +0530

    add all folder

---
+++ Project - 12 Firebase Firestore Database/PR - 12 ( Firebase Database )/src/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-xm-NOHMg9078QF17dfuikLSl73g6CFM",
+  authDomain: "crud-realtime-db-456e0.firebaseapp.com",
+  databaseURL: "https://crud-realtime-db-456e0-default-rtdb.firebaseio.com",
+  projectId: "crud-realtime-db-456e0",
+  storageBucket: "crud-realtime-db-456e0.appspot.com",
+  messagingSenderId: "791686204856",
+  appId: "1:791686204856:web:73e905e6636e12a7b0134d",
+  measurementId: "G-LRZ4DFXKY2"
+};
+
+export const app = initializeApp(firebaseConfig);
